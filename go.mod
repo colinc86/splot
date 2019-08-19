8d58b323619158dca68292fd3eda2b5f66714448
Colin Campbell <colinc86@gmail.com>
2019-08-18 19:54:54 -0500

    Initial commit.

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/colinc86/splot
+
+go 1.12
+
+require (
+	github.com/ajstarks/svgo v0.0.0-20181006003313-6ce6a3bcf6cd // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/jung-kurt/gofpdf v1.9.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20190728182440-6a916e37a237 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	golang.org/x/mobile v0.0.0-20190814143026-e8b3e6111d02 // indirect
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20190816200558-6889da9d5479 // indirect
+	gonum.org/v1/gonum v0.0.0-20190817114100-b0ff1e7714e8 // indirect
+	gonum.org/v1/netlib v0.0.0-20190331212654-76723241ea4e // indirect
+	gonum.org/v1/plot v0.0.0-20190615073203-9aa86143727f // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
---
+++ signalplotter.go
@@ -0,0 +1,52 @@
+package splot
+
+import (
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
+)
+
+// SignalPlotter represents a plotter to plot signals.
+type SignalPlotter struct {
+	Title      string
+	XAxisTitle string
+	YAxisTitle string
+}
+
+// NewSignalPlotter creates and returns a new signal plotter.
+func NewSignalPlotter(title string, xAxisTitle string, yAxisTitle string) *SignalPlotter {
+	return &SignalPlotter{
+		Title:      title,
+		XAxisTitle: xAxisTitle,
+		YAxisTitle: yAxisTitle,
+	}
+}
+
+// PlotSignal plots a signal and saves the image to a PNG file.
+func (p *SignalPlotter) PlotSignal(signal []float64, seriesTitle string, filename string) error {
+	pe, err := plot.New()
+	if err != nil {
+		return err
+	}
+
+	pe.Title.Text = p.Title
+	pe.X.Label.Text = p.XAxisTitle
+	pe.Y.Label.Text = p.YAxisTitle
+
+	plotValues := make(plotter.XYs, len(signal))
+
+	for i, v := range signal {
+		plotValues[i].X = float64(i)
+		plotValues[i].Y = v
+	}
+
+	if err := plotutil.AddLines(pe, seriesTitle, plotValues); err != nil {
+		return err
+	}
+
+	// Save the plot to a PNG file.
+	if err := pe.Save(16*vg.Inch, 8*vg.Inch, file); err != nil {
+		return err
+	}
+}
